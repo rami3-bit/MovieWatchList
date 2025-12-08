2b3271932651d6636ce62b82cd75127526551423
Rami <rami.elsaneh@protonmail.com>
2025-12-08 16:12:44 +0100

    added if statement were article goes disabled if search bar has a value

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 document.getElementById("search-btn").addEventListener("click", function() {
 	const searchBar = document.getElementById("search-bar").value
+
+	if (searchBar) { 
+        document.querySelector("article").style.display = "none";
+    }
+
 fetch(`http://www.omdbapi.com/?t=${searchBar}&apikey=270adad6`)
 	.then(res => res.json())
 	.then(data =>  {
 			console.log(data)
 					const film = `
-				Title: ${data.title}
-				Year: ${data.year}
-				Rated: ${data.rated}
-				Released: ${data.released}
-				Runtime: ${data.runtime}
+				<h3>Title: ${data.title}</h3>
+				<p>Year: ${data.year}</p>
+				<p>Rated: ${data.rated}</p>
+				<p>Released: ${data.released}</p>
+				<p>Runtime: ${data.runtime}</p>
 			`
+			
 	})
 })
