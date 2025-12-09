31af1a0e7cac0d45c6f47743717d30641e73391b
Rami <rami.elsaneh@protonmail.com>
2025-12-09 16:24:25 +0100

    adjusted design

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ document.getElementById("search-btn").addEventListener("click", function() {
 
 	if (searchBar) { 
         document.getElementById("explore").style.display = "none";
-    }
+    } 
 
-fetch(`http://www.omdbapi.com/?t=${searchBar}&apikey=270adad6`)
+fetch(`http://www.omdbapi.com/?s=${searchBar}&type=movie&apikey=270adad6`)
 	.then(res => res.json())
 	.then(data =>  {
 			console.log(data)
 
 			if (data.Response === "False") {
 				document.getElementById("search-result").innerHTML = `<p>${data.Error}</p>`;
-			} else {
-                const film = `
-                    <img src="${data.Poster}" alt="Poster of ${data.Title}" />
-                    <h3>Title: ${data.Title}</h3>
-                    <p>Year: ${data.Year}</p>
-                    <p>Rated: ${data.Rated}</p>
-                    <p>Released: ${data.Released}</p>
-                    <p>Runtime: ${data.Runtime}</p>
-                `;
-                document.getElementById("search-result").innerHTML = film;
-			}
+			} else {                let filmList = '';
 
+                // Iterera över varje film i data.Search
+                data.Search.forEach(film => {
+					
+                    filmList += `
+                        <div class="film-item">
+                            <img src="${film.Poster}" alt="Poster of ${film.Title}" style="width: 100px;">
+                            <h3>Title: ${film.Title}</h3>
+                            <p>Year: ${film.Year}</p>
+                        </div>
+                    `;
+                });
+
+                // Sätt in hela listan av filmer i search-result elementet
+                document.getElementById("search-result").innerHTML = filmList;
+			}
 	})
 })
