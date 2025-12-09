f8b4633ebb050a3683f43ac16d63a805b89f77c3
Rami <rami.elsaneh@protonmail.com>
2025-12-09 11:16:29 +0100

    added feature that wwhen wrong movie typed it gives feedback

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ fetch(`http://www.omdbapi.com/?t=${searchBar}&apikey=270adad6`)
 	.then(data =>  {
 			console.log(data)
 
-					const film = `
-				<img src="${data.Poster}" alt="Poster of ${data.Title}" />
-				<h3>Title: ${data.Title}</h3>
-				<p>Year: ${data.Year}</p>
-				<p>Rated: ${data.Rated}</p>
-				<p>Released: ${data.Released}</p>
-				<p>Runtime: ${data.Runtime}</p>
-			`
-			document.getElementById("search-result").innerHTML = film
-console.log(film)
+			if (data.Response === "False") {
+				document.getElementById("search-result").innerHTML = `<p>${data.Error}</p>`;
+			} else {
+                const film = `
+                    <img src="${data.Poster}" alt="Poster of ${data.Title}" />
+                    <h3>Title: ${data.Title}</h3>
+                    <p>Year: ${data.Year}</p>
+                    <p>Rated: ${data.Rated}</p>
+                    <p>Released: ${data.Released}</p>
+                    <p>Runtime: ${data.Runtime}</p>
+                `;
+                document.getElementById("search-result").innerHTML = film;
+			}
+
 	})
 })
