ee3775a203777ee3df37b4a39a44e714e4bcc5ec
Rami <rami.elsaneh@protonmail.com>
2025-12-09 10:44:41 +0100

    added working result with poster!

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ document.getElementById("search-btn").addEventListener("click", function() {
 	const searchBar = document.getElementById("search-bar").value
 
 	if (searchBar) { 
-        document.querySelector("article").style.display = "none";
+        document.getElementById("explore").style.display = "none";
     }
 
 fetch(`http://www.omdbapi.com/?t=${searchBar}&apikey=270adad6`)
 	.then(res => res.json())
 	.then(data =>  {
 			console.log(data)
+
 					const film = `
-				<h3>Title: ${data.title}</h3>
-				<p>Year: ${data.year}</p>
-				<p>Rated: ${data.rated}</p>
-				<p>Released: ${data.released}</p>
-				<p>Runtime: ${data.runtime}</p>
+				<img src="${data.Poster}" alt="Poster of ${data.Title}" />
+				<h3>Title: ${data.Title}</h3>
+				<p>Year: ${data.Year}</p>
+				<p>Rated: ${data.Rated}</p>
+				<p>Released: ${data.Released}</p>
+				<p>Runtime: ${data.Runtime}</p>
 			`
-			
+			document.getElementById("search-result").innerHTML = film
+console.log(film)
 	})
 })
