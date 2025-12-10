6ee7b1be2e25786b71f88b9b23c6be71838db1d6
Rami <rami.elsaneh@protonmail.com>
2025-12-10 12:37:25 +0100

    added html for watchlist.index

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ fetch(`https://www.omdbapi.com/?s=${searchBar}&type=movie&apikey=270adad6`)
                             <img src="${film.Poster}" alt="Poster of ${film.Title}" style="width: 100px;">
                             <h3>Title: ${film.Title}</h3>
                             <p>Year: ${film.Year}</p>
+							<p>Year: ${film.ImdbRating}</p>
                         </div>
                     `;
                 });
