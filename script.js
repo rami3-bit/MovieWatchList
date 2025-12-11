7a1c5929bc150419bdc46b090e6e49b15d6ffe84
Rami <rami.elsaneh@protonmail.com>
2025-12-11 15:28:01 +0100

    removed some uneccesary files and design change

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById("search-btn").addEventListener("click", function() {
 	const searchBar = document.getElementById("search-bar").value
 
@@ -28,7 +30,7 @@ fetch(`https://www.omdbapi.com/?s=${searchBar}&type=movie&apikey=270adad6`)
                     filmList += `
                         <div class="film-item">
                             <div class="film-poster">
-                                <img src="${film.Poster}" alt="Poster of ${film.Title}" style="width: 100px;">
+                                <img class="film-poster-img" src="${film.Poster}" alt="Poster of ${film.Title}" style="width: 100px;">
                             </div>
                             <div class="film-details">
                                 <h3>${film.Title}</h3>
@@ -50,7 +52,7 @@ fetch(`https://www.omdbapi.com/?s=${searchBar}&type=movie&apikey=270adad6`)
 						
                         console.log("clicked")
 				})
-			})
+				})
             });
 		}
 	})
