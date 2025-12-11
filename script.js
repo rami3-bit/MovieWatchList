fda03d2f585b66535f4eebe14a0e48870a10d6c2
Rami <rami.elsaneh@protonmail.com>
2025-12-11 12:00:57 +0100

    added details for films api

--- script.js
+++ script.js
@@ -12,28 +12,38 @@ fetch(`https://www.omdbapi.com/?s=${searchBar}&type=movie&apikey=270adad6`)
 
 			if (data.Response === "False") {
 				document.getElementById("search-result").innerHTML = `<p>${data.Error}</p>`;
-			} else {                let filmList = '';
+			} else {                
 
-                // Iterera över varje film i data.Search
-                data.Search.forEach(film => {
-					
+  // Gör API-anrop för varje film för att få FULL data
+            const promises = data.Search.map(film => 
+                fetch(`https://www.omdbapi.com/?i=${film.imdbID}&apikey=270adad6`)
+                    .then(res => res.json())
+            );
+
+            // Vänta på ALLA svar innan du visar HTML
+            Promise.all(promises).then(films => {
+                let filmList = '';
+
+                films.forEach(film => {
                     filmList += `
-                            <div class="film-item">
-								<div class="film-poster">
-                                	<img src="${film.Poster}" alt="Poster of ${film.Title}" style="width: 100px;">
-								</div>
-								<div class="film-details">
-                                	<h3>${film.Title}</h3>
-                                	<p> ⭐ ${film.imdbRating}</p>
-                                	<p>Length: ${film.Runtime}</p>
-									<p>Add watchlist + </p>
-                                	<p>Description: ${film.Plot}</p>
-								</div>
+                        <div class="film-item">
+                            <div class="film-poster">
+                                <img src="${film.Poster}" alt="Poster of ${film.Title}" style="width: 100px;">
                             </div>
-							<hr>
+                            <div class="film-details">
+                                <h3>${film.Title}</h3>
+                                <p> ⭐ ${film.imdbRating}</p>
+                                <p>Length: ${film.Runtime}</p>
+                                <button> Add watchlist + </button>
+                                <p>Description: ${film.Plot}</p>
+                            </div>
+                        </div>
+                        <hr>
                     `;
-					document.getElementById("search-result").innerHTML = filmList;
                 });
+
+                document.getElementById("search-result").innerHTML = filmList;
+            });
 			}
 	})
 })
