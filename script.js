db194bd27af4400a2eb386376ca9f106d02fa2fb
Rami <rami.elsaneh@protonmail.com>
2025-12-15 14:06:29 +0100

    added local storage for watshlist

--- script.js
+++ script.js
@@ -34,12 +34,29 @@ fetch(`https://www.omdbapi.com/?s=${searchBar}&type=movie&apikey=270adad6`)
                             <div class="film-poster">
                                 <img class="film-poster-img" src="${posterUrl}" alt="Poster of ${film.Title}" style="width: 100px;" onerror="this.src='https://dummyimage.com/300x450/cccccc/969696?text=No+Poster'">
                             </div>
+
                             <div class="film-details">
-                                <h3>${film.Title}</h3>
-                                <p> ⭐ ${film.imdbRating}</p>
-                                <p>Length: ${film.Runtime}</p>
-                                <button class="add-watchlist-btn"> Add watchlist + </button>
-                                <p>Description: ${film.Plot}</p>
+                                <h3>
+									${film.Title}
+								</h3>
+                                <p>
+								 	⭐${film.imdbRating}
+								 </p>
+                                <p>
+									Length: ${film.Runtime}
+								</p>
+                                <button class="add-watchlist-btn"
+									data-id="${film.imdbID}"
+  									data-title="${film.Title}"
+  									data-poster="${posterUrl}"
+  									data-rating="${film.imdbRating}"
+  									data-runtime="${film.Runtime}"
+  									data-plot="${film.Plot}">
+  									Add +
+								</button>
+                                <p>
+									Description: ${film.Plot}
+								</p>
                             </div>
                         </div>
                         <hr>
@@ -47,13 +64,28 @@ fetch(`https://www.omdbapi.com/?s=${searchBar}&type=movie&apikey=270adad6`)
                 });
                 document.getElementById("search-result").
 				innerHTML = filmList;
+
 			//lägga till watchlist
+				if (!localStorage.getItem("WatchListKey")) {
+    				localStorage.setItem("WatchListKey", JSON.stringify([]));
+				}
+
 				document.querySelectorAll(".add-watchlist-btn").forEach(btn => {
 					btn.addEventListener("click", function() {
-						const watchList = {}
-						
-                        console.log("clicked")
-					
+					const watchListObj = {
+						imdbID: this.dataset.id,
+						title: this.dataset.title,
+						rating: this.dataset.rating,
+    					runtime: this.dataset.runtime,
+    					plot: this.dataset.plot
+					}
+					  
+        			let watchlist = JSON.parse(localStorage.getItem("WatchListKey")) || [];
+       				 watchlist.push(watchListObj);
+        			localStorage.setItem("WatchListKey", JSON.stringify(watchlist));
+
+        			alert(`${watchListObj.title} has been added to your watchlist!`);
+						console.log(watchListObj)
 					})
 				})
             });
